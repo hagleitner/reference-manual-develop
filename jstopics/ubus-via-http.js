9126aa7bbf668f174b965b23be6cead3738f1cfa
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-08-30 11:21:26 +0200

    updated documentation

--- jstopics/ubus-via-http.js
+++ jstopics/ubus-via-http.js
@@ -8,5 +8,5 @@ hmParentLink:"hsmd-gwlan-configurationinterfaces.html",
 hmBreadCrumbs:"<a href=\"hsmd.html\">HsM devices<\/a> &gt; <a href=\"hsmd-gwlan.html\">HsM Gateway LAN<\/a> &gt; <a href=\"hsmd-gwlan-configurationinterfaces.html\">Configuration Interfaces<\/a>",
 hmTitlePath:"HsM devices > HsM Gateway LAN > Configuration Interfaces > ubus via HTTP",
 hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">ubus via HTTP<\/span><\/h1>\n\r",
-hmBody:"<p class=\"p_Normal\">The gateway has the <span class=\"f_CodeExample\">uhttpd<\/span> plugin installed that allows ubus calls using HTTP protocol. The ubus URL is <span class=\"f_CodeExample\">&lt;address of device&gt;\/ubus<\/span>. While usually not relevant for the operator of the gateway the ubus over HTTP protocoll is used by the Xibu App when running the gateway configuration. <\/p>\n\r"
+hmBody:"<p class=\"p_Normal\">The gateway has the <span class=\"f_CodeExample\">uhttpd<\/span> plugin installed that allows <a href=\"https:\/\/openwrt.org\/docs\/techref\/ubus\" target=\"_blank\" class=\"weblink\">ubus<\/a> calls using HTTP protocol. The ubus URL is <span class=\"f_CodeExample\">&lt;address of device&gt;\/ubus<\/span>. While usually not relevant for the operator of the gateway the ubus over HTTP protocoll is used by the Xibu App when running the gateway configuration. <\/p>\n\r"
 })
