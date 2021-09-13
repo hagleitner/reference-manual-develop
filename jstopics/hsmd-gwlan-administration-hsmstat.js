de6e1682579500a30063630f93fb85a6232182f1
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-09-13 07:34:26 +0200

    updated documentation

--- jstopics/hsmd-gwlan-administration-hsmstat.js
+++ jstopics/hsmd-gwlan-administration-hsmstat.js
@@ -8,5 +8,5 @@ hmParentLink:"hsmd-gwlan-administration.html",
 hmBreadCrumbs:"<a href=\"hsmd.html\">HsM devices<\/a> &gt; <a href=\"hsmd-gwlan.html\">HsM Gateway LAN<\/a> &gt; <a href=\"hsmd-gwlan-administration.html\">Administration<\/a>",
 hmTitlePath:"HsM devices > HsM Gateway LAN > Administration > hsmstat",
 hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">hsmstat<\/span><\/h1>\n\r",
-hmBody:"<p class=\"p_Normal\">The gateway offers the shell command <span class=\"f_CodeExample\">hsmstat<\/span> which will collect some information that may be useful for support. The information is presented as JSON file.<\/p>\n\r<p class=\"p_Normal\">You can make this command available via the gateway\'s web page using custom commands.<\/p>\n\r"
+hmBody:"<p class=\"p_Normal\">The gateway offers the shell command <span class=\"f_CodeExample\">hsmstat<\/span> which will collect some information that may be useful for support. The information is presented as JSON file.<\/p>\n\r<p class=\"p_Normal\">You can make this command available via the gateway\'s web page using custom commands or execute it via SSH.<\/p>\n\r"
 })
